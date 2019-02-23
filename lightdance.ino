344c21b76d05947b39665c32f8997cd43f17183d
pranav1711999m <47632404+pranav1711999m@users.noreply.github.com>
2019-02-24 01:50:04 +0530

    ADDED NEW PIN

--- lightdance.ino
+++ lightdance.ino
@@ -1,59 +1,59 @@
-/*allon - full light
-  alloff -full off
-  h-head
-  la-left arm
-  ra -right arm
-  lh- lefthip
-  ra-right arm
-  lh - left hip
-  rh - right hip
-  ha- both hand
-  ll - left leg
-  rl - right leg
-  lf - left foot
-  rf - right foot
-  hi - both hip
-  f - both foot
-  rlc - right hand&left leg
-  lrc - left hand & right leg
-  cc - both leg and hand
-  hfc - head & hand
-  al - all left
-  ar - all right
-  bl - both leg
-*/
- int a[12]={5,6,7,8,9,10,11,12,13,14,15};
 
+/*allon - full light
+ alloff -full off
+ h-head
+ la-left arm
+ ra -right arm
+ lh- lefthip
+ ra-right arm
+ lh - left hip
+ rh - right hip
+ ha- both hand
+ ll - left leg
+ rl - right leg
+ lf - left foot
+ rf - right foot
+ hi - both hip
+ f - both foot
+rlc - right hand&left leg
+ lrc - left hand & right leg
+ cc - both leg and hand
+ hfc - head & hand
+ al - all left
+ ar - all right
+ bl - both leg
+*/
   void allon(){
-  digitalWrite(13, HIGH);
-  digitalWrite(12, HIGH);
-  digitalWrite(11, HIGH);
-  digitalWrite(10, HIGH);
-  digitalWrite(9, HIGH);
-  digitalWrite(8, HIGH);
-  digitalWrite(7, HIGH);
-  digitalWrite(6, HIGH);
-  digitalWrite(5, HIGH);
-  digitalWrite(4, HIGH);
-  digitalWrite(3, HIGH);
+  
+  digitalWrite(10,1);
+  digitalWrite(9,1);
+  digitalWrite(8,1);
+  digitalWrite(7,1);
+  digitalWrite(6,1);
+  digitalWrite(5,1);
+  digitalWrite(4,1);
+  digitalWrite(3,1);
+  digitalWrite(2,1);
 
 
   }
   void alloff(){
-  digitalWrite(13, LOW);
-  digitalWrite(12, LOW);
-  digitalWrite(11, LOW);
-  digitalWrite(10, LOW);
-  digitalWrite(9, LOW);
-  digitalWrite(8, LOW);
-  digitalWrite(7, LOW);
-  digitalWrite(6, LOW);
-  digitalWrite(5, LOW);
-  digitalWrite(4, LOW);
-  digitalWrite(3, LOW);
+  
+  digitalWrite(10,0);
+  digitalWrite(9,0);
+  digitalWrite(8,0);
+  digitalWrite(7,0);
+  digitalWrite(6,0);
+  digitalWrite(5,0);
+  digitalWrite(4,0);
+  digitalWrite(3,0);
+  digitalWrite(2,0);
 }
-
-  void allrun(){
+  void allrun()
+  
+  {
+    
+ int a[12]={5,6,7,8,9,10,11,12,13,14,15};
   for (int i=0; i < sizeof(a)/sizeof(a[0]);i++)
  {
   digitalWrite(a[i],1);
@@ -66,94 +66,187 @@ void h(){
   int h;
   h=1;
   {
-    digitalWrite(7,1);
+    digitalWrite(4,1);
     delay(100);
-    digitalWrite(7,0);
+    digitalWrite(4,0);
     delay(100);
   }
 }
 void ha(){
   int ha=1;
   {
-    digitalWrite(5,1);
-    digitalWrite(6,1);
-    delay(1000);
-    digitalWrite(5,0);
-    digitalWrite(6,0);
-    delay(1000);   
+    digitalWrite(2,1);
+    digitalWrite(3,1);
+    delay(100);
+    digitalWrite(2,0);
+    digitalWrite(3,0);
+    delay(100);   
   }
 }
 
 void la()
   {
-  digitalWrite(5,1);
-  delay(1000);
-  digitalWrite(5,0);
-  delay(1000);  
+  digitalWrite(2,1);
+  delay(100);
+  digitalWrite(2,0);
+  delay(100);  
 }
 void ra()
 {
-  digitalWrite(6,1);
-  delay(1000);
-    digitalWrite(6,0);
-    delay(1000);
+  digitalWrite(3,1);
+  delay(100);
+    digitalWrite(3,0);
+    delay(100);
 }
 void hi()
 {
-    digitalWrite(8,1);
-      digitalWrite(9,1);
-    delay(1000);
-      digitalWrite(8,0);
-        digitalWrite(9,0);
-      delay(1000);  
+    digitalWrite(5,1);
+      digitalWrite(6,1);
+    delay(100);
+      digitalWrite(5,0);
+        digitalWrite(6,0);
+      delay(100);  
 }
 
 void lh()
 {
-  digitalWrite(8,1);
-  delay(1000);
-   digitalWrite(8,0);
-   delay(1000);
+  digitalWrite(5,1);
+  delay(100);
+   digitalWrite(5,0);
+   delay(100);
     
 }
 void rh()
 {
-    digitalWrite(9,1);
-    delay(1000);
-      digitalWrite(9,0);
-      delay(1000);
+    digitalWrite(6,1);
+    delay(100);
+      digitalWrite(6,0);
+      delay(100);
 }  
 void ll()
 {
-    digitalWrite(10,1);
-    delay(1000);
-      digitalWrite(10,0);
-      delay(1000);
+    digitalWrite(7,1);
+    delay(100);
+      digitalWrite(7,0);
+      delay(100);
 }
 void rl()
 {
-   digitalWrite(11,1);
-   delay(1000);
-    digitalWrite(11,0);
-    delay(1000);
+   digitalWrite(8,1);
+   delay(100);
+    digitalWrite(8,0);
+    delay(100);
+}
+
+void bl()
+{
+  digitalWrite(7,1);
+  digitalWrite(8,1);
+  delay(100);
+  digitalWrite(7,0);
+  digitalWrite(8,0);
+  delay(100);
 }
 void lf()
 {
-   digitalWrite(12,1);
-   delay(1000);
-    digitalWrite(12,0);
-    delay(1000);
+   digitalWrite(9,1);
+   delay(100);
+    digitalWrite(9,0);
+    delay(100);
 }
 void rf()
 {
-   digitalWrite(13,1);
-   delay(1000);
-    digitalWrite(13,0);
-    delay(1000);
+   digitalWrite(10,1);
+   delay(100);
+    digitalWrite(10,0);
+    delay(100);
 }
+void f()
+{
+  digitalWrite(9,1);
+    digitalWrite(10,1);
+    delay(100);
+      digitalWrite(9,0);
+        digitalWrite(10,0);
+        delay(100);
+  
+}
+void rlc()
+{
+   digitalWrite(3,1);
+    digitalWrite(7,1);
+   delay(100);
+   digitalWrite(3,0);
+    digitalWrite(7,0);
+    delay(100);
+}
+void lrc()
+{
+   digitalWrite(2,1);
+    digitalWrite(8,1);
+    delay(100);
+    digitalWrite(2,0);
+    digitalWrite(8,0);
+    delay(100);
+}
+    void cc()
+    {
+      digitalWrite(2,1);
+      digitalWrite(3,1);
+          digitalWrite(5,1);
+              digitalWrite(6,1);
+      delay(100);
+     digitalWrite(2,0);
+      digitalWrite(3,0);
+          digitalWrite(5,0);
+              digitalWrite(6,0);
+      delay(100);
+    }
+    void hfc()
+    {
+      digitalWrite(2,1);
+      digitalWrite(3,1);
+          digitalWrite(9,1);
+              digitalWrite(10,1);
+              delay(100);
+              digitalWrite(2,0);
+      digitalWrite(3,0);
+          digitalWrite(9,0);
+              digitalWrite(6,0);
+              delay(100);
+    }
+    void al()
+    {
+      digitalWrite(2,1);
+      digitalWrite(5,1);
+          digitalWrite(7,1);
+              digitalWrite(9,1);
+              delay(100);
+              digitalWrite(2,0);
+      digitalWrite(5,0);
+          digitalWrite(7,0);
+              digitalWrite(9,0);
+              delay(100);
+              
+    }
+    void ar()
+    {
+      digitalWrite(3,1);
+      digitalWrite(6,1);
+          digitalWrite(8,1);
+              digitalWrite(10,1);
+              delay(100);
+              digitalWrite(3,0);
+      digitalWrite(6,0);
+          digitalWrite(8,0);
+              digitalWrite(10,0);
+              delay(100);
+    }
+    
+
 void setup()
 {
-pinMode(5, OUTPUT);
+  pinMode(5, OUTPUT);
 pinMode(6, OUTPUT);
 pinMode(7, OUTPUT);
 pinMode(8, OUTPUT);
@@ -175,21 +268,54 @@ void loop()
   delay(1000);
   {alloff();}
   delay(1000);
-  {allrun();}
-  delay(1000);
-  {h();}
-  delay(1000);
+   {allrun();}
+   delay(50);
+   {allrun();}
+   delay(100);
+ {h();}
+  delay(100);
   {ra();}
+  delay(100);
   {la();}
-  delay(1000);
+  delay(100);
   {lh();}
+  delay(100);
   {rh();}
-  delay(1000);
-  {rl();}
+  delay(100);
+ 
   {ll();}
-  delay(1000);
+  delay(100);
+   {rl();}
+  delay(100);
+    {lf();}
+  delay(100);
+
   {rf();}
-  {lf();}
-  delay(1000); 
+  delay(100);
+  {ha();}
+  delay(100);
+  {hi();}
+  delay(100);
+  {bl();}
+  delay(100);
+  {f();}
+  delay(100);
+{cc();}
+delay(100);
+ {rlc();}
+delay(100); 
+{lrc();}
+delay(100);
+{hfc();}
+delay(100);
+{al();}
+delay(100);
+{ar();}
+delay(100);
 }
 
+
+
+
+
+  
